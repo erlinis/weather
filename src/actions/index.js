fbae049f243b1529ce63f53c8876fca301fc6758
Erlinis Quintana <erlinis.quintana@gmail.com>
2018-01-08 21:04:17 -0500

    Creating an ajax request and using redux promises

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'ea61204fc606fdb5bbd581eb8c1617d6';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const COUNTRY_CODE = 'co';
+  const url = `${ROOT_URL}&q=${city},${COUNTRY_CODE}`;
+  const request =  axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
---
+++ src/containers/search_bar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect }from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather} from '../actions/index';
+
+
+class SearchBar extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit  = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event){
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event){
+    event.preventDefault();
+
+    // Fetch weather data here
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+  }
+
+  render(){
+   return (
+     <form onSubmit={this.onFormSubmit} className="input-group">
+       <input
+         placeholder="Get a 5-day forecast in the entered city"
+         className="form-control"
+         value={this.state.term}
+         onChange={this.onInputChange}
+       />
+       <span className="input-group-btn">
+         <button type="submit" className="btn btn-secondary"> Search </button>
+       </span>
+     </form>
+    )
+  }
+}
+
+// Everytime an action is called and returned, it ensure the action result flows trought the middleware and then to all reducers
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
