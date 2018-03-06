6b079596851cbd5d64f474c060ed61cc358a6f85
Erlinis Quintana <erlinis.quintana@koombea.com>
2018-03-06 18:58:42 -0500

    Adding temperature sparkline graphic

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
 
   renderWeather(cityData){
-
     const name = cityData.city.name
+    const temperatures = cityData.list.map( weather => weather.main.temp);
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={100} width={160} data={temperatures}>
+            <SparklinesLine color="red"/>
+          </Sparklines>
+        </td>
       </tr>
     );
   }
